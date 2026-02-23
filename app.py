16d20c5ea293eda48310cc70d083ccf0853149aa
Aniruddha Sarkar <aniruddhas387@gmail.com>
2026-02-23 10:18:05 +0530

    SHAP Update

--- app.py
+++ app.py
@@ -206,8 +206,8 @@ def add_header(response):
     response.headers['Expires'] = '-1'
     return response
 
-@app.errorhandler(404)
-def page_not_found(e): return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e): return render_template('404.html'), 404
 
 # @app.errorhandler(500)
 # def internal_server_error(e): return render_template('500.html'), 500
